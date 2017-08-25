ad0b30a41e4a7e7248d5aac7eaae4659dad4338d
Ilya Abrashkin <iliacmd@yandex.ru>
2017-08-25 10:39:39 +0300

    Download special hubs and save json_js.file

--- scripts/download.py
+++ scripts/download.py
@@ -1,34 +1,54 @@
 #!/usr/local/bin/python3
 
 import requests
-import xml.etree.ElementTree as ET
-import xml.dom.minidom
-import os.path
 import json
 import xmltodict
+import shutil
+import time
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+listHub = [
+    'programming',
+    'mobile_dev'
+]
+
+rssUrl = "https://habrahabr.ru/rss/hub"
 
 xml_content = ''
 
-if ( not os.path.isfile('download.xml')):
-	response = requests.get("https://habrahabr.ru/rss/hub/mobile_dev/", verify=False)
-	if (response.status_code == 200):
-		xml_content = response.text
-		file = open('donload.xml', 'w')
-		if (xml_content):
-			file.write(xml_content)
-			file.close()
+file = open('json_file.js', 'w')
+file.write(';var json_file = (function(){ return [')
+
+if(file):
+
+    for hubName in listHub:
+
+        json_block = ' { "name" : "' + hubName + '", "data" : '
+
+        fullUrl = rssUrl + '/' + hubName + '/best/alltime/'
+
+        response = requests.get(fullUrl, verify=False)
+
+        print('Забираем: ' + fullUrl)
+
+        if (response.status_code == 200):
+            xml_content = response.text
+            if (xml_content):
+                parsed_xml = xmltodict.parse(xml_content)
+                json_block += json.dumps(parsed_xml) + ' },'
+                file.write(json_block)
+                json_block = ''
+                print('Пишем в файл...')
+
+    time.sleep(2)
 
 
+file.write(']; })();')
+file.close()
 
-file = open('download.xml', 'r')
-xml_string = file.read()
-#dom = xml.dom.minidom.parse('donload.xml')
-o = xmltodict.parse(xml_string)
+shutil.move('json_file.js', '../src/json_file.js')
 
-#items = dom.getElementsByTagName("item")
-
-# for item in items:
-# 	title = item.getElementsByTagName('title')
-# 	print(title)
-
-print(';var json_file = (function(){ return ' + json.dumps(o) + ';})();')
+#print(';var json_file = (function(){ return ' + json_content + ';})();')
